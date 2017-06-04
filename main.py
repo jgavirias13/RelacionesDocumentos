ec11f1ed6b8a1da5244776135b2110fd69f0ecc0
fmontoy <fmontoyalvarez@gmail.com>
2017-06-04 17:13:32 -0500

    Comentarios

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def main():
     argumentos = parser.parse_args()
     if argumentos.PATH:
         listaArchivos = ls(argumentos.PATH) #Tomar todos los archivos de la carpeta
-        cantidadDocumentos = len(listaArchivos)
+        cantidadDocumentos = len(listaArchivos) #Cantidad de archivos en la carpeta
         for nombreArchivo in listaArchivos:
             ruta = ruta_completa(argumentos.PATH, nombreArchivo) #Tomar la ruta a cada archivo
             documento = open(ruta, 'r') #Abrir el archivo
